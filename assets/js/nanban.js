2e4b3bbb9a058699195fff351196edf42c7b0fab
uwo-omich <94110231+uwo-omich@users.noreply.github.com>
2021-11-25 15:12:11 +0500

    Update nanban.js

--- assets/js/nanban.js
+++ assets/js/nanban.js
@@ -1,4 +1,7 @@
-let pre_npc_eu_amount = 0;
-let pre_ea_amount = 0;
-let max_cargo = 3000;
-let post_npc_eu_amount = 0;
+function nanban_calc() {
+	let pre_npc_eu_amount = parseInt(document.getElementById("pre_npc_eu_amount").value);
+	let pre_ea_amount = parseInt(document.getElementById("pre_ea_amount").value);
+	let max_cargo = parseInt(document.getElementById("max_cargo").value);
+	let post_npc_eu_amount = pre_npc_eu_amount / (pre_ea_amount / (max_cargo / 3));
+	document.getElementById("result").value = "You should trade " + post_npc_eu_amount + " EA items";
+}
