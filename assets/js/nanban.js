ab27a11f2ebef6e433411c9c9d35417a969ea355
uwo-omich <94110231+uwo-omich@users.noreply.github.com>
2021-11-25 15:22:21 +0500

    Update nanban.js

--- assets/js/nanban.js
+++ assets/js/nanban.js
@@ -3,6 +3,12 @@ function nanban_calc() {
 	let pre_ea_amount = parseInt(document.getElementById("pre_ea_amount").value);
 	let max_cargo = parseInt(document.getElementById("max_cargo").value);
 	let post_npc_eu_amount = pre_npc_eu_amount / (pre_ea_amount / (max_cargo / 3));
-	let result_string = "You should trade " + post_npc_eu_amount.toFixed() + " EA items";
+	let result_string = "You should trade " + post_npc_eu_amount.toFixed() + " EU goods.";
+	if (post_npc_eu_amount > pre_npc_eu_amount) {
+		result_string += " Add " + (post_npc_eu_amount - pre_npc_eu_amount).toFixed() + " EU goods to get full cargo.";
+	}
+	if (post_npc_eu_amount < pre_npc_eu_amount) {
+		result_string += " You may save " + (pre_npc_eu_amount - post_npc_eu_amount).toFixed() + " units before the trade.";
+	}
 	document.getElementById("result").innerText = result_string;
 }
